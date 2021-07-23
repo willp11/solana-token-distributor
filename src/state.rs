5c8e2cb07c98b8015c7f60f780a2777ad59bfa38
unknown <williampage11@yahoo.co.uk>
2021-07-23 18:30:40 +0700

    added state, pack and unpack for LockupSchedule state

--- src/state.rs
+++ src/state.rs
@@ -34,6 +34,7 @@ impl Pack for LockupSchedule {
     fn unpack_from_slice(src: &[u8]) -> Result<Self, ProgramError> {
         let src = array_ref![src, 0, LockupSchedule::LEN];
         let (
+            is_initialized,
             token_mint,
             start_timestamp,
             number_periods,
@@ -46,6 +47,47 @@ impl Pack for LockupSchedule {
             [1] => true,
             _ => return Err(TokenDistributorError::InvalidLockupScheduleData.into());
         }
+
+        Ok(LockupSchedule {
+            is_initialized,
+            token_mint: Pubkey::new_from_array(*token_mint),
+            start_timestamp: u64::from_le_bytes(*start_timestamp),
+            number_periods: u32::from_le_bytes(*number_periods),
+            period_duration: u64::from_le_bytes(*period_duration),
+            total_token_quantity: u64::from_le_bytes(*total_token_quantity),
+            token_quantity_locked: u64::from_le_bytes(*token_quantity_locked)
+        })
+    }
+
+    fn pack_into_slice(&self, dstL &mut [u8]) {
+        let dst = array_mut_ref![dst, 0, LockupSchedule::LEN];
+        let (
+            is_initialized_dst,
+            token_mint_dst,
+            start_timestamp_dst,
+            number_periods_dst,
+            period_duration_dst,
+            total_token_quantity_dst,
+            token_quantity_locked_dst
+        ) = mut_array_refs![dst, 1, 4, 8, 4, 8, 8, 8];
+
+        let LockupSchedule {
+            is_initialized,
+            token_mint,
+            start_timestamp,
+            number_periods,
+            period_duration,
+            total_token_quantity,
+            token_quantity_locked
+        } = self;
+
+        is_initialized_dst[0] = *is_initialized as u8;
+        token_mint_dst.copy_from_slice(token_mint.as_ref());
+        *start_timestamp_dst = start_timestamp.to_le_bytes();
+        *number_periods_dst = number_periods.to_le_bytes();
+        *period_duration_dst = period_duration.to_le_bytes();
+        *total_token_quantity_dst = total_token_quantity.to_le_bytes();
+        *token_quantity_locked_dst = token_quantity_locked.to_le_bytes();
     }
 
 }
