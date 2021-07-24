afdc1e43cafb939b1fba6d127b469d02eb52c2b3
unknown <williampage11@yahoo.co.uk>
2021-07-24 11:05:36 +0700

    fixed all errors in state.rs

--- src/state.rs
+++ src/state.rs
@@ -10,13 +10,13 @@ use crate::error::TokenDistributorError;
 
 // LOCKUP SCHEDULE STATE
 pub struct LockupSchedule {
-    is_initialized: bool,
-    token_mint: Pubkey,
-    start_timestamp: u64,
-    number_periods: u32,
-    period_duration: u64,
-    total_token_quantity: u64,
-    token_quantity_locked: u64
+    pub is_initialized: bool,
+    pub token_mint: Pubkey,
+    pub start_timestamp: u64,
+    pub number_periods: u32,
+    pub period_duration: u64,
+    pub total_token_quantity: u64,
+    pub token_quantity_locked: u64
 }
 
 impl Sealed for LockupSchedule {}
@@ -28,8 +28,8 @@ impl IsInitialized for LockupSchedule {
 }
 
 impl Pack for LockupSchedule {
-    // is_intialized=1, mint=4, start_timestamp=8, number_periods=4, duration=8, total_quantity=8, quantity_locked=8
-    const LEN: usize = 41;
+    // is_intialized=1, mint=32, start_timestamp=8, number_periods=4, duration=8, total_quantity=8, quantity_locked=8
+    const LEN: usize = 69;
 
     fn unpack_from_slice(src: &[u8]) -> Result<Self, ProgramError> {
         let src = array_ref![src, 0, LockupSchedule::LEN];
@@ -41,12 +41,12 @@ impl Pack for LockupSchedule {
             period_duration,
             total_token_quantity,
             token_quantity_locked
-        ) = array_refs![src, 1, 4, 8, 4, 8, 8, 8];
+        ) = array_refs![src, 1, 32, 8, 4, 8, 8, 8];
         let is_initialized = match is_initialized {
             [0] => false,
             [1] => true,
-            _ => return Err(TokenDistributorError::InvalidLockupScheduleData.into());
-        }
+            _ => return Err(TokenDistributorError::InvalidLockupScheduleData.into())
+        };
 
         Ok(LockupSchedule {
             is_initialized,
@@ -59,7 +59,7 @@ impl Pack for LockupSchedule {
         })
     }
 
-    fn pack_into_slice(&self, dstL &mut [u8]) {
+    fn pack_into_slice(&self, dst: &mut [u8]) {
         let dst = array_mut_ref![dst, 0, LockupSchedule::LEN];
         let (
             is_initialized_dst,
@@ -69,7 +69,7 @@ impl Pack for LockupSchedule {
             period_duration_dst,
             total_token_quantity_dst,
             token_quantity_locked_dst
-        ) = mut_array_refs![dst, 1, 4, 8, 4, 8, 8, 8];
+        ) = mut_array_refs![dst, 1, 32, 8, 4, 8, 8, 8];
 
         let LockupSchedule {
             is_initialized,
@@ -93,12 +93,12 @@ impl Pack for LockupSchedule {
 
 // LOCKUP STATE
 pub struct Lockup {
-    is_initialized: bool,
-    lockup_schedule_state: Pubkey,
-    receiving_account: Pubkey,
-    lockup_token_account: Pubkey,
-    token_quantity: u64,
-    periods_redeemed: u32
+    pub is_initialized: bool,
+    pub lockup_schedule_state: Pubkey,
+    pub receiving_account: Pubkey,
+    pub lockup_token_account: Pubkey,
+    pub token_quantity: u64,
+    pub periods_redeemed: u32
 }
 
 impl Sealed for Lockup {}
@@ -110,8 +110,8 @@ impl IsInitialized for Lockup {
 }
 
 impl Pack for Lockup {
-    // is_intialized=1, lockup_schedule_state=4, receiving_account=4, lockup_token_account=4, token_quantity=8, periods_redeemed=4
-    const LEN: usize = 25;
+    // is_intialized=1, lockup_schedule_state=32, receiving_account=32, lockup_token_account=32, token_quantity=8, periods_redeemed=4
+    const LEN: usize = 109;
 
     fn unpack_from_slice(src: &[u8]) -> Result<Self, ProgramError> {
         let src = array_ref![src, 0, Lockup::LEN];
@@ -122,12 +122,12 @@ impl Pack for Lockup {
             lockup_token_account,
             token_quantity,
             periods_redeemed
-        ) = array_refs![src, 1, 4, 4, 4, 8, 4];
+        ) = array_refs![src, 1, 32, 32, 32, 8, 4];
         let is_initialized = match is_initialized {
             [0] => false,
             [1] => true,
-            _ => return Err(TokenDistributorError::InvalidLockupScheduleData.into());
-        }
+            _ => return Err(TokenDistributorError::InvalidLockupScheduleData.into())
+        };
 
         Ok(Lockup {
             is_initialized,
@@ -139,7 +139,7 @@ impl Pack for Lockup {
         })
     }
 
-    fn pack_into_slice(&self, dstL &mut [u8]) {
+    fn pack_into_slice(&self, dst: &mut [u8]) {
         let dst = array_mut_ref![dst, 0, Lockup::LEN];
         let (
             is_initialized_dst,
@@ -148,9 +148,9 @@ impl Pack for Lockup {
             lockup_token_account_dst,
             token_quantity_dst,
             periods_redeemed_dst
-        ) = mut_array_refs![dst, 1, 4, 4, 4, 8, 4];
+        ) = mut_array_refs![dst, 1, 32, 32, 32, 8, 4];
 
-        let LockupSchedule {
+        let Lockup {
             is_initialized,
             lockup_schedule_state,
             receiving_account,
