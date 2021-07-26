a7c68b348cfd4c7fae1d96945cfaeac18b96bc61
unknown <williampage11@yahoo.co.uk>
2021-07-26 13:11:37 +0700

    removed unused import

--- js-ui/src/util/createLockupSchedule.js
+++ js-ui/src/util/createLockupSchedule.js
@@ -1,4 +1,3 @@
-import { AccountLayout, Token, TOKEN_PROGRAM_ID } from "@solana/spl-token";
 import { Keypair, Connection, PublicKey, SystemProgram, SYSVAR_RENT_PUBKEY, SYSVAR_CLOCK_PUBKEY, Transaction, TransactionInstruction } from "@solana/web3.js";
 import BN from "bn.js";
 import {LOCKUP_SCHEDULE_ACCOUNT_DATA_LAYOUT} from '../util/layout';
